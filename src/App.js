e31b8028256770645f93f90c8839edb96de90d47
Nicollas Prudencio <nicollas_ap37@hotmail.com>
2023-10-10 10:58:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ function App() {
         <p>
           UI 2
           <br />
-          Esta é uma pagina de testes em React!
+          Esta é uma pagina de testes em React hospedada na VPS 2 da IBCMED!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.ibcmed.com.br/deploy/index.html"
           target="_blank"
           rel="noopener noreferrer"
         >
